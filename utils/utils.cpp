cb3a52883ca7b58b11acef90e5b2ca70690c016c
He Peng <he.peng@ygomi.com>
2017-10-20 17:19:45 +0800

    add utils function

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -28,4 +28,47 @@ std::string Time2string(uint64_t utc_ms)
     return str;
 }
 
+void split(const std::string& s, const std::string& delim, std::vector<std::string>& ret)
+{
+    size_t last = 0;
+    size_t index=s.find_first_of(delim,last);
+    while (index!=std::string::npos) {
+        ret.push_back(s.substr(last,index-last));
+        last=index+ delim.length();
+        index=s.find_first_of(delim,last);
+    }
+    if(last < s.length()) {
+        ret.push_back(s.substr(last));
+    }
+}
+
+time_t  my_gmktime(register struct tm *t)
+/* struct tm to seconds since Unix epoch */
+{
+    register   long  year;
+    register   time_t  result;
+#define  MONTHSPERYEAR   12       /* months per calendar year */
+    static   const   int  cumdays[MONTHSPERYEAR] =
+    { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 };
+    /*@ +matchanyintegral @*/
+    year = 1900 + t->tm_year + t->tm_mon / MONTHSPERYEAR;
+    result = (year - 1970) * 365 + cumdays[t->tm_mon % MONTHSPERYEAR];
+    result += (year -  1968) / 4;
+    result -= (year -  1900) / 100;
+    result += (year -  1600) / 400;
+    if  ((year % 4) == 0 && ((year % 100) != 0 || (year % 400) == 0) && (t->tm_mon % MONTHSPERYEAR) < 2) {
+        result-- ;
+    }
+    result += t->tm_mday -  1;
+    result *= 24;
+    result += t->tm_hour;
+    result *= 60;
+    result += t->tm_min;
+    result *= 60;
+    result += t->tm_sec;
+    if  (t->tm_isdst == 1)
+        result -= 3600;
+    /*@ - matchanyintegral @*/
+    return  (result);
+}
 
--- utils/utils.hpp
+++ utils/utils.hpp
@@ -10,8 +10,20 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <memmory>
+
+template <typename T>
+std::shared_ptr<T> make_shared_array(size_t size)
+{
+
+    return std::shared_ptr<T>(new T[size], std::default_delete<T[]>());
+
+}
 
 std::string Time2string(uint64_t utc_ms);
 
+void split(const std::string& s, const std::string& delim, std::vector<std::string>& ret);
+
+time_t  my_gmktime(register struct tm *t);
 
 
